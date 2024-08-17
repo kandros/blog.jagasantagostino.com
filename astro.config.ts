f23c6d54ecd36d59bb14c5e0f08b41ca6a374798
Jaga Santagostino <jagasantagostino@gmail.com>
2024-08-17 04:10:28 +0200

    .

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,7 @@
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig, envField } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
+import { defineConfig, envField } from "astro/config";
 
 import mdx from "@astrojs/mdx";
 
@@ -17,9 +17,9 @@ export default defineConfig({
       schema: {
         NOTION_API_KEY: envField.string({
           context: "server",
-          access: "secret"
-        })
-      }
-    }
-  }
+          access: "secret",
+        }),
+      },
+    },
+  },
 });
