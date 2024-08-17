a5fda2d2c38982e46ca8ad6ef3d92dfe192089a7
Jaga Santagostino <jagasantagostino@gmail.com>
2024-08-17 04:10:13 +0200

    .

--- astro.config.ts
+++ astro.config.ts
@@ -1,21 +1,25 @@
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig, envField } from "astro/config";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.jagasantagostino.com",
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), mdx()],
   output: "hybrid",
   adapter: vercel(),
   experimental: {
     serverIslands: true,
     env: {
       schema: {
-        NOTION_API_KEY: envField.string({ context: "server", access: "secret" }),
-      },
-    },
-  },
+        NOTION_API_KEY: envField.string({
+          context: "server",
+          access: "secret"
+        })
+      }
+    }
+  }
 });
